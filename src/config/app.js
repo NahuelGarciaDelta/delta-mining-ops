515ee1b86005aa59f0e0c0ee2a51e89fd14ff17f
NahuelGarciaDelta <nahuel.garcia@deltamining.com.ar>
2026-08-14 16:01:56 -0300

    Update app.js

--- src/config/app.js
+++ src/config/app.js
@@ -1,3 +1,3 @@
 export const APPS_SCRIPT_URL =
   import.meta.env.VITE_APPS_SCRIPT_URL ||
-  "https://script.google.com/macros/s/AKfycbxHyZPSNlYFI0LhMhdeByEfYygtMvA-sVEFpCaMAvCLpjLt8VnhWNB2t0cz7mmUomH9/exec";
+  "https://script.google.com/macros/s/AKfycbxU-ihsxXTNn2wa5EO1OkSM5FjJ43MwxSx8dY0RjbnJRFBKF0BiNNq7QsuohWxmmeOhog/exec";
