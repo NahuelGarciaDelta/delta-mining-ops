a3fbc782d1d87b7e7cda0d8808a3f33e4c4656c7
NahuelGarciaDelta <nahuel.garcia@deltamining.com.ar>
2026-09-09 01:43:50 -0300

    Use historical data wrapper for executive dashboard

--- src/modules/home/index.js
+++ src/modules/home/index.js
@@ -1,3 +1,3 @@
 export { default as ViewBienvenida } from "./ViewBienvenidaProjectFilter.jsx";
 
-export { default as ExecutiveDashboard } from "./ExecutiveDashboard.jsx";
+export { default as ExecutiveDashboard } from "./ExecutiveDashboardHistorical.jsx";
